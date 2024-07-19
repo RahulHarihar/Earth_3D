fdb1fd22aebaac07b1ee6440940a67a39b9d2522
unknown <rahulharihar71@gmail.com>
2024-07-19 23:04:36 +0530

    Made it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       |<div className="container">
         <h1>Earth</h1>
         <p>Earth is the third planet from the Sun and the only astronomical object known to harbor life.<br/>
-           This is enabled by Earth being an ocean world, the only one in the Solar System sustaining liquid surface water.
+           This is enabled by Earth being an ocean world, the only one in the Solar System sustaining liquid surface water.<br/>
            Almost all of Earth's water is contained in its global ocean, covering 70.8% of Earth's crust.</p>
       </div>
     </>
